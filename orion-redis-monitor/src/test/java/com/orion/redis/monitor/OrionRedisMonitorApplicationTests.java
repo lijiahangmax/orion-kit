b09a22d1c0683007269aca2607d63248ba0c867f
lijiahang <ljh1553488six@qq.com>
2020-05-15 18:28:52 +0800

    init redis monitor project

---
+++ orion-redis-monitor/src/test/java/com/orion/redis/monitor/OrionRedisMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orion.redis.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionRedisMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
