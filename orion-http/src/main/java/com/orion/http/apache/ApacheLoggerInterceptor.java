7c6636b45ee0e66233b86161e6dc5dbac7c593e1
lijiahang <ljh1553488six@139.com>
2021-04-09 18:30:18 +0800

    review code

--- orion-http/src/main/java/com/orion/http/apache/ApacheLoggerInterceptor.java
+++ orion-http/src/main/java/com/orion/http/apache/ApacheLoggerInterceptor.java
@@ -23,6 +23,18 @@ public class ApacheLoggerInterceptor implements HttpRequestInterceptor, HttpResp
 
     private static final ThreadLocal<Args.Four<String, String, Long, String>> HOLDER = new ThreadLocal<>();
 
+    private static final String DEF = "Apache-HTTP-Request ";
+
+    private String suffix;
+
+    public ApacheLoggerInterceptor() {
+        this(DEF);
+    }
+
+    public ApacheLoggerInterceptor(String suffix) {
+        this.suffix = suffix;
+    }
+
     @Override
     public void process(HttpRequest httpRequest, HttpContext httpContext) {
         long start = System.currentTimeMillis();
@@ -31,7 +43,7 @@ public class ApacheLoggerInterceptor implements HttpRequestInterceptor, HttpResp
         String uri = request.getUri();
         String traceId = UUIds.random32();
         HOLDER.set(Args.of(method, uri, start, traceId));
-        LOGGER.info("Apache-HTTP-Request START method: [{}], url: [{}], start: [{}], traceId: [{}]", method, uri, start, traceId);
+        LOGGER.info(suffix + "START method: [{}], url: [{}], start: [{}], traceId: [{}]", method, uri, start, traceId);
     }
 
     @Override
@@ -41,6 +53,15 @@ public class ApacheLoggerInterceptor implements HttpRequestInterceptor, HttpResp
         HOLDER.remove();
         StatusLine response = httpResponse.getStatusLine();
         int code = response.getStatusCode();
-        LOGGER.info("Apache-HTTP-Request END [use: {}ms], code: {}, success: {}, method: [{}], url: [{}], traceId: [{}]", end - p.getArg3(), code, code >= 200 && code < 300, p.getArg1(), p.getArg2(), p.getArg4());
+        LOGGER.info(suffix + "END [use: {}ms], code: {}, success: {}, method: [{}], url: [{}], traceId: [{}]", end - p.getArg3(), code, code >= 200 && code < 400, p.getArg1(), p.getArg2(), p.getArg4());
     }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
 }
--- orion-http/src/main/java/com/orion/http/ok/OkLoggerInterceptor.java
+++ orion-http/src/main/java/com/orion/http/ok/OkLoggerInterceptor.java
@@ -23,22 +23,42 @@ public class OkLoggerInterceptor implements Interceptor {
      */
     private static final Logger LOGGER = LoggerFactory.getLogger(OkLoggerInterceptor.class);
 
+    private static final String DEF = "OK-Request ";
+
+    private String suffix;
+
+    public OkLoggerInterceptor() {
+        this(DEF);
+    }
+
+    public OkLoggerInterceptor(String suffix) {
+        this.suffix = suffix;
+    }
+
     @Override
     public Response intercept(Interceptor.Chain chain) throws IOException {
         Request request = chain.request();
         long start = System.currentTimeMillis();
         String traceId = UUIds.random32();
-        LOGGER.info("OK-Request START method: [{}], url: [{}], tag: [{}], traceId: [{}]", request.method(), request.url(), request.tag(), traceId);
+        LOGGER.info(suffix + "START method: [{}], url: [{}], tag: [{}], traceId: [{}]", request.method(), request.url(), request.tag(), traceId);
         try {
             Response response = chain.proceed(request);
             long end = System.currentTimeMillis();
-            LOGGER.info("OK-Http-Request END [use: {}ms], code: {}, success: {}, method: [{}], url: [{}], tag: [{}], traceId: [{}]", end - start, response.code(), response.isSuccessful(), request.method(), request.url(), request.tag(), traceId);
+            LOGGER.info(suffix + "END [use: {}ms], code: {}, success: {}, method: [{}], url: [{}], tag: [{}], traceId: [{}]", end - start, response.code(), response.isSuccessful(), request.method(), request.url(), request.tag(), traceId);
             return response;
         } catch (IOException e) {
             long end = System.currentTimeMillis();
-            LOGGER.error("OK-Http-Request ERROR [use: {}ms], method: [{}], url: [{}], tag: [{}], traceId: [{}], errorMessage: [{}]", end - start, request.method(), request.url(), request.tag(), traceId, e.getMessage());
+            LOGGER.error(suffix + "ERROR [use: {}ms], method: [{}], url: [{}], tag: [{}], traceId: [{}], errorMessage: [{}]", end - start, request.method(), request.url(), request.tag(), traceId, e.getMessage());
             throw e;
         }
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
     }
 
 }
