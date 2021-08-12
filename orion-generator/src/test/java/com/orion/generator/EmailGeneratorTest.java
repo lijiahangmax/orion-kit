f7ded06d688089a3456d1a7ef47d9291a889beca
lijiahang <ljh1553488six@139.com>
2021-08-12 12:49:24 +0800

    feat: generator email

---
+++ orion-generator/src/test/java/com/orion/generator/EmailGeneratorTest.java
@@ -0,0 +1,27 @@
+package com.orion.generator;
+
+import com.orion.generator.email.EmailGenerator;
+import org.junit.Test;
+
+/**
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2021/8/12 12:41
+ */
+public class EmailGeneratorTest {
+
+    @Test
+    public void test1() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(EmailGenerator.generatorRealEmail());
+        }
+    }
+
+    @Test
+    public void test2() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(EmailGenerator.generatorEmail());
+        }
+    }
+
+}
