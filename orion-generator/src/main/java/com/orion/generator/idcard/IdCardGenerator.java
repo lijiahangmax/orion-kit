bbaa93802482bbefdfa2cfb0c4174a101d12764f
lijiahang <ljh1553488six@139.com>
2021-08-17 18:19:06 +0800

    add id card generator.

---
+++ orion-generator/src/main/java/com/orion/generator/idcard/IdCardGenerator.java
@@ -0,0 +1,237 @@
+package com.orion.generator.idcard;
+
+import com.orion.generator.addres.AddressGenerator;
+import com.orion.generator.addres.AddressSupport;
+import com.orion.utils.Arrays1;
+import com.orion.utils.Strings;
+import com.orion.utils.identity.IdCards;
+import com.orion.utils.random.Randoms;
+import com.orion.utils.time.Birthdays;
+import com.orion.utils.time.DateStream;
+import com.orion.utils.time.Dates;
+
+import java.util.Date;
+
+/**
+ * 身份证生成器
+ *
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2021/8/11 14:38
+ */
+public class IdCardGenerator {
+
+    private static final int[] MAN_GENDER_CODE, WOMAN_GENDER_CODE;
+
+    private static final int AGE_MIN, AGE_MAX;
+
+    private static final String PSB;
+
+    private IdCardGenerator() {
+    }
+
+    static {
+        MAN_GENDER_CODE = new int[]{1, 3, 5, 7, 9};
+        WOMAN_GENDER_CODE = new int[]{0, 2, 4, 6, 8};
+        AGE_MIN = 18;
+        AGE_MAX = 50;
+        PSB = "公安局";
+    }
+
+    /**
+     * 随机生成一个身份证信息 [18 ~ 50] 岁
+     *
+     * @return 身份证
+     */
+    public static String generator() {
+        return generator(AGE_MIN, AGE_MAX);
+    }
+
+    public static String generator(int ageMin, int ageMax) {
+        return generator(AddressSupport.randomCountyCode().toString(), ageMin, ageMax, Randoms.randomBoolean());
+    }
+
+    public static String generator(String code, int ageMin, int ageMax) {
+        return generator(code, ageMin, ageMax, Randoms.randomBoolean());
+    }
+
+    public static String generator(int ageMin, int ageMax, boolean gender) {
+        return generator(AddressSupport.randomCountyCode().toString(), ageMin, ageMax, gender);
+    }
+
+    /**
+     * 生成身份证
+     *
+     * @param code   code 6位
+     * @param ageMin ageMin [
+     * @param ageMax ageMax ]
+     * @param gender true男
+     * @return 身份证
+     */
+    public static String generator(String code, int ageMin, int ageMax, boolean gender) {
+        return generator(code, Randoms.randomInt(ageMin, ageMax + 1), gender);
+    }
+
+    public static String generator(int age) {
+        return generator(AddressSupport.randomCountyCode().toString(), age, Randoms.randomBoolean());
+    }
+
+    public static String generator(String code, int age) {
+        return generator(code, age, Randoms.randomBoolean());
+    }
+
+    public static String generator(int age, boolean gender) {
+        return generator(AddressSupport.randomCountyCode().toString(), age, gender);
+    }
+
+    /**
+     * 生成身份证
+     *
+     * @param code   code 6位
+     * @param age    age
+     * @param gender true男
+     * @return 身份证
+     */
+    public static String generator(String code, int age, boolean gender) {
+        return generator(code, Birthdays.generatorBirthday(age), gender);
+    }
+
+    public static String generator(String birthday) {
+        return generator(AddressSupport.randomCountyCode().toString(), birthday, Randoms.randomBoolean());
+    }
+
+    public static String generator(String birthday, boolean gender) {
+        return generator(AddressSupport.randomCountyCode().toString(), birthday, gender);
+    }
+
+    public static String generator(String code, String birthday) {
+        return generator(code, birthday, Randoms.randomBoolean());
+    }
+
+    /**
+     * 生成身份证
+     *
+     * @param code     code 6位
+     * @param birthday 生日 8位
+     * @param gender   true男
+     * @return 身份证
+     */
+    public static String generator(String code, String birthday, boolean gender) {
+        // 顺序码
+        String seq = Strings.leftPad(Randoms.randomInt(100) + Strings.EMPTY, 2, "0");
+        // 性别
+        int genderCode = Arrays1.random(gender ? MAN_GENDER_CODE : WOMAN_GENDER_CODE);
+        String idCard17 = code + birthday + seq + genderCode;
+        // 检查码
+        char checkCode = IdCards.getCheckCode18(idCard17);
+        return idCard17 + checkCode;
+    }
+
+    /**
+     * 获取签发机关
+     *
+     * @param idCard idCard
+     * @return 签发机关
+     */
+    public static String getIssueOrg(String idCard) {
+        String county = idCard.substring(0, 6);
+        return AddressSupport.getSupportCountyName(Integer.parseInt(county)) + PSB;
+    }
+
+    /**
+     * 获取县地址
+     *
+     * @param idCard idCard
+     * @return 县地址
+     */
+    public static String getAddress(String idCard) {
+        String county = idCard.substring(0, 6);
+        return AddressSupport.getCountyAddress(AddressSupport.getSupportCountyCode(Integer.parseInt(county)));
+    }
+
+    /**
+     * 获取详细地址
+     *
+     * @param idCard idCard
+     * @return 详细地址
+     */
+    public static String getFullAddress(String idCard) {
+        String county = idCard.substring(0, 6);
+        Integer countyCode = AddressSupport.getSupportCountyCode(Integer.parseInt(county));
+        return AddressSupport.getCountyAddress(countyCode) + AddressGenerator.generatorIdCardAddress();
+    }
+
+    /**
+     * 获取县编码
+     *
+     * @param idCard idCard
+     * @return 县编码
+     */
+    public static Integer getAddressCode(String idCard) {
+        return Integer.valueOf(idCard.substring(0, 6));
+    }
+
+    /**
+     * 获取编码
+     *
+     * @param idCard idCard
+     * @return 省, 市, 县
+     */
+    public static Integer[] getAddressCodeExt(String idCard) {
+        String province = idCard.substring(0, 2);
+        String city = idCard.substring(0, 4);
+        String county = idCard.substring(0, 6);
+        return new Integer[]{Integer.valueOf(province), Integer.valueOf(city), Integer.valueOf(county)};
+    }
+
+    /**
+     * 获取地址
+     *
+     * @param idCard idCard
+     * @return 省, 市, 县
+     */
+    public static String[] getAddressExt(String idCard) {
+        return AddressSupport.getAddressExt(getAddressCode(idCard));
+    }
+
+    /**
+     * 获取有效期限
+     *
+     * @param idCard idCard
+     * @return yyyyMMdd-yyyyMMdd
+     */
+    public static String getPeriodString(String idCard) {
+        Date[] dates = getPeriod(idCard);
+        return Dates.format(dates[0], Dates.YMD2) + "-" + Dates.format(dates[1], Dates.YMD2);
+    }
+
+    /**
+     * 获取有效期限
+     *
+     * @param idCard idCard
+     * @return yyyyMMdd-yyyyMMdd
+     */
+    public static Date[] getPeriod(String idCard) {
+        int age = IdCards.getAge(idCard);
+        int periodYear;
+        if (age <= 16) {
+            periodYear = 5;
+        } else if (age <= 25) {
+            periodYear = 10;
+        } else {
+            periodYear = 20;
+        }
+        DateStream curr = Dates.stream();
+        // 年
+        int year = curr.getYear() - (age / 8);
+        // 随机月
+        int month = Randoms.randomInt(12) + 1;
+        // 随机日
+        int day = Randoms.randomInt(Dates.getMonthLastDay(year, month)) + 1;
+        DateStream period = Dates.stream(Dates.build(year, month, day));
+        Date start = period.date();
+        Date end = period.addYear(periodYear).date();
+        return new Date[]{start, end};
+    }
+
+}
---
+++ orion-generator/src/test/java/com/orion/generator/IdCardGeneratorTest.java
@@ -0,0 +1,51 @@
+package com.orion.generator;
+
+import com.orion.generator.idcard.IdCardGenerator;
+import com.orion.utils.Valid;
+import com.orion.utils.identity.IdCards;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2021/8/12 10:22
+ */
+public class IdCardGeneratorTest {
+
+    @Test
+    public void generator() {
+        for (int i = 0; i < 20; i++) {
+            String c = IdCardGenerator.generator(18);
+            System.out.print(c + " ");
+            System.out.print(IdCards.getAge(c) + " ");
+            System.out.print(IdCards.getGender(c) ? "男 " : "女 ");
+            System.out.print(IdCardGenerator.getIssueOrg(c) + " ");
+            System.out.print(IdCardGenerator.getPeriodString(c) + " ");
+            System.out.print(IdCardGenerator.getAddress(c) + " ");
+            System.out.println();
+            Valid.isTrue(IdCards.isValidCard18(c));
+            Valid.isTrue(IdCards.getAge(c) == 18);
+        }
+    }
+
+    @Test
+    public void generator1() {
+        for (int i = 0; i < 20; i++) {
+            String c = IdCardGenerator.generator(5, 50);
+            System.out.print(c + " ");
+            System.out.print(IdCards.getAge(c) + " ");
+            System.out.print(IdCards.getGender(c) ? "男 " : "女 ");
+            System.out.print(IdCardGenerator.getIssueOrg(c) + " ");
+            System.out.print(IdCardGenerator.getPeriodString(c) + " ");
+            System.out.print(IdCardGenerator.getFullAddress(c) + " ");
+            System.out.print(IdCardGenerator.getAddressCode(c) + " ");
+            System.out.print(Arrays.toString(IdCardGenerator.getAddressCodeExt(c)) + " ");
+            System.out.print(Arrays.toString(IdCardGenerator.getAddressExt(c)) + " ");
+            System.out.println();
+            Valid.isTrue(IdCards.isValidCard18(c));
+        }
+    }
+
+}
