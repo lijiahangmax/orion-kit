9c3d490cc95e8e61a194467fe0f5fa6f83cc7013
lijiahang <ljh1553488six@139.com>
2021-09-07 19:20:22 +0800

    add: rc4 encrypt.

---
+++ orion-lang/src/test/java/com/orion/test/encrypt/Rc4Tests.java
@@ -0,0 +1,21 @@
+package com.orion.test.encrypt;
+
+import com.orion.utils.crypto.RC4;
+import org.junit.Test;
+
+/**
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2021/9/3 23:02
+ */
+public class Rc4Tests {
+
+    @Test
+    public void test() {
+        RC4 r = new RC4("12443");
+        String xxx = r.encrypt("xxx");
+        System.out.println(xxx);
+        System.out.println(r.decrypt(xxx));
+    }
+
+}
