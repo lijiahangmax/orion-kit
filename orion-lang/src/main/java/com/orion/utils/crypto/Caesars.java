f4267d9655fcf99b773ae6d65a6dff001be61f53
lijiahang <ljh1553488six@139.com>
2021-09-22 15:59:56 +0800

    add caesar encrypt.

---
+++ orion-lang/src/main/java/com/orion/utils/crypto/Caesars.java
@@ -0,0 +1,81 @@
+package com.orion.utils.crypto;
+
+/**
+ * 凯撒密码实现
+ *
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2021/9/4 1:08
+ */
+public class Caesars {
+
+    public static final String TABLE = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+    private int keys;
+
+    public Caesars() {
+        this(3);
+    }
+
+    public Caesars(int keys) {
+        this.keys = keys;
+    }
+
+    /**
+     * 加密
+     *
+     * @param s 明文
+     * @return 密文
+     */
+    public String encrypt(String s) {
+        char[] plain = s.toCharArray();
+        for (int i = 0; i < plain.length; i++) {
+            if (!Character.isLetter(plain[i])) {
+                continue;
+            }
+            plain[i] = cipher(plain[i]);
+        }
+        return new String(plain);
+    }
+
+    /**
+     * 解密
+     *
+     * @param s 密文
+     * @return 明文
+     */
+    public String decrypt(String s) {
+        char[] plain = s.toCharArray();
+        for (int i = 0; i < plain.length; i++) {
+            if (!Character.isLetter(plain[i])) {
+                continue;
+            }
+            plain[i] = decCipher(plain[i]);
+        }
+        return new String(plain);
+    }
+
+    /**
+     * 加密轮盘
+     *
+     * @param str str
+     * @return 密文
+     */
+    private char cipher(char str) {
+        int position = (TABLE.indexOf(str) + keys) % 52;
+        return TABLE.charAt(position);
+    }
+
+    /**
+     * 解密轮盘
+     *
+     * @param str str
+     * @return 明文
+     */
+    private char decCipher(char str) {
+        int position = (TABLE.indexOf(str) - keys) % 52;
+        position = position < 0 ? 52 + position : position;
+        return TABLE.charAt(position);
+    }
+
+}
---
+++ orion-lang/src/test/java/com/orion/test/encrypt/CaesarTests.java
@@ -0,0 +1,22 @@
+package com.orion.test.encrypt;
+
+import com.orion.utils.crypto.Caesars;
+import org.junit.Test;
+
+/**
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2021/9/4 1:11
+ */
+public class CaesarTests {
+
+    @Test
+    public void test1() {
+        Caesars caesars = new Caesars();
+        String e = caesars.encrypt("kdqoijiwqo43");
+        System.out.println(e);
+        String d = caesars.decrypt(e);
+        System.out.println(d);
+    }
+
+}
