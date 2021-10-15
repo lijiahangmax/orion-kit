486d2b73e79b182b2b51a7aff1eb2b9568eb518d
lijiahang <ljh1553488six@139.com>
2021-10-15 22:50:16 +0800

    feat: add compress enum.

---
+++ orion-lang/src/main/java/com/orion/utils/io/compress/CompressType.java
@@ -0,0 +1,29 @@
+package com.orion.utils.io.compress;
+
+import java.util.function.Supplier;
+
+/**
+ * 压缩类型
+ *
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2021/9/27 15:52
+ */
+public interface CompressType<C extends FileCompressor, D extends FileDecompressor> {
+
+    /**
+     * @return 压缩器
+     */
+    Supplier<C> compressor();
+
+    /**
+     * @return 解压器
+     */
+    Supplier<D> decompressor();
+
+    /**
+     * @return 后缀
+     */
+    String suffix();
+
+}
