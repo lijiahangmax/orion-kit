edcd629df3559181a5b1825b2d1dbd41568455b8
lijiahang <ljh1553488six@139.com>
2021-10-23 23:47:14 +0800

    review param working mode symmetric.

---
+++ orion-lang/src/main/java/com/orion/utils/crypto/symmetric/ParamSymmetric.java
@@ -0,0 +1,81 @@
+package com.orion.utils.crypto.symmetric;
+
+import com.orion.utils.Exceptions;
+import com.orion.utils.Strings;
+import com.orion.utils.Valid;
+import com.orion.utils.crypto.enums.CipherAlgorithm;
+import com.orion.utils.crypto.enums.PaddingMode;
+import com.orion.utils.crypto.enums.WorkingMode;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import java.security.spec.AlgorithmParameterSpec;
+
+import static com.orion.utils.codec.Base64s.decode;
+import static com.orion.utils.codec.Base64s.encode;
+
+/**
+ * CBC CFB OFB FTP GCM 模式非对称加密 AES DES 3DES
+ *
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2020/11/3 14:45
+ */
+public class ParamSymmetric extends BaseSymmetric {
+
+    /**
+     * 参数规格
+     */
+    private AlgorithmParameterSpec paramSpec;
+
+    /**
+     * aad
+     */
+    private byte[] aad;
+
+    public ParamSymmetric(CipherAlgorithm cipherAlgorithm, WorkingMode workingMode, SecretKey secretKey, AlgorithmParameterSpec paramSpec) {
+        this(cipherAlgorithm, workingMode, PaddingMode.PKCS5_PADDING, secretKey, paramSpec);
+    }
+
+    public ParamSymmetric(CipherAlgorithm cipherAlgorithm, WorkingMode workingMode, PaddingMode paddingMode, SecretKey secretKey, AlgorithmParameterSpec paramSpec) {
+        super(cipherAlgorithm, workingMode, paddingMode, secretKey);
+        this.paramSpec = Valid.notNull(paramSpec, "paramSpec is null");
+    }
+
+    public void setAad(String aad) {
+        this.aad = Strings.bytes(aad);
+    }
+
+    public void setAad(byte[] aad) {
+        this.aad = aad;
+    }
+
+    @Override
+    public byte[] encrypt(byte[] plain) {
+        try {
+            Cipher cipher = super.getCipher();
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, paramSpec);
+            if (aad != null) {
+                cipher.updateAAD(aad);
+            }
+            return encode(cipher.doFinal(this.zeroPadding(plain, cipher.getBlockSize())));
+        } catch (Exception e) {
+            throw Exceptions.encrypt("encrypt data error", e);
+        }
+    }
+
+    @Override
+    public byte[] decrypt(byte[] text) {
+        try {
+            Cipher cipher = super.getCipher();
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, paramSpec);
+            if (aad != null) {
+                cipher.updateAAD(aad);
+            }
+            return this.clearZeroPadding(cipher.doFinal(decode(text)));
+        } catch (Exception e) {
+            throw Exceptions.encrypt("decrypt data error", e);
+        }
+    }
+
+}
