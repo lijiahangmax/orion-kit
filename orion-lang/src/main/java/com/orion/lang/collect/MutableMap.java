8e7046024ac616079f2e571c43d0b2d781a15615
lijiahang <ljh1553488six@139.com>
2022-01-23 14:38:49 +0800

    add mutable map interface.

--- orion-lang/src/main/java/com/orion/lang/collect/MutableMap.java
+++ orion-lang/src/main/java/com/orion/lang/collect/MutableMap.java
@@ -1,9 +1,6 @@
 package com.orion.lang.collect;
 
-import com.orion.lang.mutable.MutableString;
-import com.orion.lang.wrapper.Args;
-import com.orion.lang.wrapper.Pair;
-import com.orion.utils.Arrays1;
+import com.orion.function.*;
 import com.orion.utils.Valid;
 import com.orion.utils.convert.Converts;
 import com.orion.utils.math.BigDecimals;
@@ -13,483 +10,420 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.*;
-import java.util.function.Function;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.*;
 
 /**
- * 可转换的Map
+ * 可转换的 map
  *
  * @author Jiahang Li
  * @version 1.0.0
  * @since 2021/1/5 17:20
  */
-@SuppressWarnings("ALL")
 public interface MutableMap<K, V> extends Map<K, V> {
 
-    default void puts(Args.Entry<K, V>... es) {
-        int length = Arrays1.length(es);
-        for (int i = 0; i < length; i++) {
-            put(es[i].getKey(), es[i].getValue());
-        }
-    }
-
-    default void puts(Map.Entry<K, V>... es) {
-        int length = Arrays1.length(es);
-        for (int i = 0; i < length; i++) {
-            put(es[i].getKey(), es[i].getValue());
-        }
-    }
-
-    default void puts(Pair<K, V>... es) {
-        int length = Arrays1.length(es);
-        for (int i = 0; i < length; i++) {
-            put(es[i].getKey(), es[i].getValue());
-        }
-    }
-
-    default List<V> gets(K... ks) {
-        List<V> list = new ArrayList<>();
-        int length = Arrays1.length(ks);
-        for (int i = 0; i < length; i++) {
-            list.add(get(ks[i]));
-        }
-        return list;
-    }
-
     default Byte getByte(K k) {
-        V v = get(k);
-        if (v == null) {
-            return null;
-        }
-        return Converts.toByte(v);
+        return this.getByte(k, Suppliers.nullSupplier());
     }
 
     default Byte getByte(K k, Byte def) {
+        return this.getByte(k, () -> def);
+    }
+
+    default Byte getByte(K k, Supplier<Byte> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toByte(v);
     }
 
     default byte getByteValue(K k) {
-        V v = get(k);
-        if (v == null) {
-            return 0;
-        }
-        return Converts.toByte(v);
+        return this.getByteValue(k, Suppliers.BYTE_SUPPLIER);
     }
 
     default byte getByteValue(K k, byte def) {
+        return this.getByteValue(k, () -> def);
+    }
+
+    default byte getByteValue(K k, ByteSupplier supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.getAsByte();
         }
         return Converts.toByte(v);
     }
 
     default Short getShort(K k) {
-        V v = get(k);
-        if (v == null) {
-            return null;
-        }
-        return Converts.toShort(k);
+        return this.getShort(k, Suppliers.nullSupplier());
     }
 
     default Short getShort(K k, Short def) {
+        return this.getShort(k, () -> def);
+    }
+
+    default Short getShort(K k, Supplier<Short> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toShort(k);
     }
 
     default short getShortValue(K k) {
-        V v = get(k);
-        if (v == null) {
-            return 0;
-        }
-        return Converts.toShort(k);
+        return this.getShortValue(k, Suppliers.SHORT_SUPPLIER);
     }
 
     default short getShortValue(K k, short def) {
+        return this.getShortValue(k, () -> def);
+    }
+
+    default short getShortValue(K k, ShortSupplier supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.getAsShort();
         }
         return Converts.toShort(k);
     }
 
-    default Integer getInt(K k) {
-        V v = get(k);
-        if (v == null) {
-            return null;
-        }
-        return Converts.toInt(k);
+    default Integer getInteger(K k) {
+        return this.getInteger(k, Suppliers.nullSupplier());
     }
 
-    default Integer getInt(K k, Integer def) {
+    default Integer getInteger(K k, Integer def) {
+        return this.getInteger(k, () -> def);
+    }
+
+    default Integer getInteger(K k, Supplier<Integer> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toInt(k);
     }
 
     default int getIntValue(K k) {
-        V v = get(k);
-        if (v == null) {
-            return 0;
-        }
-        return Converts.toInt(k);
+        return this.getIntValue(k, Suppliers.INT_SUPPLIER);
     }
 
     default int getIntValue(K k, int def) {
+        return this.getIntValue(k, () -> def);
+    }
+
+    default int getIntValue(K k, IntSupplier supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.getAsInt();
         }
         return Converts.toInt(k);
     }
 
     default Long getLong(K k) {
-        V v = get(k);
-        if (v == null) {
-            return null;
-        }
-        return Converts.toLong(v);
+        return this.getLong(k, Suppliers.nullSupplier());
     }
 
     default Long getLong(K k, Long def) {
+        return this.getLong(k, () -> def);
+    }
+
+    default Long getLong(K k, Supplier<Long> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toLong(v);
     }
 
     default long getLongValue(K k) {
-        V v = get(k);
-        if (v == null) {
-            return 0L;
-        }
-        return Converts.toLong(v);
+        return this.getLongValue(k, Suppliers.LONG_SUPPLIER);
     }
 
     default long getLongValue(K k, long def) {
+        return this.getLongValue(k, () -> def);
+    }
+
+    default long getLongValue(K k, LongSupplier supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.getAsLong();
         }
         return Converts.toLong(v);
     }
 
     default Float getFloat(K k) {
-        V v = get(k);
-        if (v == null) {
-            return null;
-        }
-        return Converts.toFloat(v);
+        return this.getFloat(k, Suppliers.nullSupplier());
     }
 
     default Float getFloat(K k, Float def) {
+        return this.getFloat(k, () -> def);
+    }
+
+    default Float getFloat(K k, Supplier<Float> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toFloat(v);
     }
 
     default float getFloatValue(K k) {
-        V v = get(k);
-        if (v == null) {
-            return 0F;
-        }
-        return Converts.toFloat(v);
+        return this.getFloatValue(k, Suppliers.FLOAT_SUPPLIER);
     }
 
     default float getFloatValue(K k, float def) {
+        return this.getFloatValue(k, () -> def);
+    }
+
+    default float getFloatValue(K k, FloatSupplier supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.getAsFloat();
         }
         return Converts.toFloat(v);
     }
 
     default Double getDouble(K k) {
-        V v = get(k);
-        if (v == null) {
-            return null;
-        }
-        return Converts.toDouble(v);
+        return this.getDouble(k, Suppliers.nullSupplier());
     }
 
     default Double getDouble(K k, Double def) {
+        return this.getDouble(k, () -> def);
+    }
+
+    default Double getDouble(K k, Supplier<Double> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toDouble(v);
     }
 
     default double getDoubleValue(K k) {
-        V v = get(k);
-        if (v == null) {
-            return 0.0;
-        }
-        return Converts.toDouble(v);
+        return this.getDoubleValue(k, Suppliers.DOUBLE_SUPPLIER);
     }
 
     default double getDoubleValue(K k, double def) {
+        return this.getDoubleValue(k, () -> def);
+    }
+
+    default double getDoubleValue(K k, DoubleSupplier supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.getAsDouble();
         }
         return Converts.toDouble(v);
     }
 
     default Boolean getBoolean(K k) {
-        V v = get(k);
-        if (v == null) {
-            return null;
-        }
-        return Converts.toBoolean(v);
+        return this.getBoolean(k, Suppliers.nullSupplier());
     }
 
     default Boolean getBoolean(K k, Boolean def) {
+        return this.getBoolean(k, () -> def);
+    }
+
+    default Boolean getBoolean(K k, Supplier<Boolean> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toBoolean(v);
     }
 
     default boolean getBooleanValue(K k) {
-        V v = get(k);
-        if (v == null) {
-            return false;
-        }
-        return Converts.toBoolean(v);
+        return this.getBooleanValue(k, Suppliers.BOOLEAN_SUPPLIER);
     }
 
     default boolean getBooleanValue(K k, boolean def) {
+        return this.getBooleanValue(k, () -> def);
+    }
+
+    default boolean getBooleanValue(K k, BooleanSupplier supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.getAsBoolean();
         }
         return Converts.toBoolean(v);
     }
 
-    default Character getChar(K k) {
-        V v = get(k);
-        if (v == null) {
-            return null;
-        }
-        return Converts.toChar(v);
+    default Character getCharacter(K k) {
+        return this.getCharacter(k, Suppliers.nullSupplier());
     }
 
-    default Character getChar(K k, Character def) {
+    default Character getCharacter(K k, Character def) {
+        return this.getCharacter(k, () -> def);
+    }
+
+    default Character getCharacter(K k, Supplier<Character> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toChar(v);
     }
 
     default char getCharValue(K k) {
-        V v = get(k);
-        if (v == null) {
-            return 0;
-        }
-        return Converts.toChar(v);
+        return this.getCharValue(k, Suppliers.CHAR_SUPPLIER);
     }
 
     default char getCharValue(K k, char def) {
+        return this.getCharValue(k, () -> def);
+    }
+
+    default char getCharValue(K k, CharSupplier supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.getAsChar();
         }
         return Converts.toChar(v);
     }
 
     default String getString(K k) {
-        return Converts.toString(get(k));
+        return this.getString(k, Suppliers.nullSupplier());
     }
 
     default String getString(K k, String def) {
+        return this.getString(k, () -> def);
+    }
+
+    default String getString(K k, Supplier<String> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toString(v);
     }
 
-    default MutableString getMutableString(K k) {
-        return new MutableString(Converts.toString(get(k)));
-    }
-
-    default MutableString getMutableString(K k, String def) {
-        V v = get(k);
-        if (v == null) {
-            return new MutableString(def);
-        }
-        return new MutableString(Converts.toString(v));
-    }
-
-    default MutableString getMutableString(K k, MutableString def) {
-        V v = get(k);
-        if (v == null) {
-            return def;
-        }
-        return new MutableString(Converts.toString(v));
-    }
-
     default Date getDate(K k) {
-        return Converts.toDate(get(k));
+        return this.getDate(k, Suppliers.nullSupplier());
     }
 
     default Date getDate(K k, Date def) {
+        return this.getDate(k, () -> def);
+    }
+
+    default Date getDate(K k, Supplier<Date> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toDate(v);
     }
 
     default LocalDateTime getLocalDateTime(K k) {
-        return Converts.toLocalDateTime(get(k));
+        return this.getLocalDateTime(k, Suppliers.nullSupplier());
     }
 
     default LocalDateTime getLocalDateTime(K k, LocalDateTime def) {
+        return this.getLocalDateTime(k, () -> def);
+    }
+
+    default LocalDateTime getLocalDateTime(K k, Supplier<LocalDateTime> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toLocalDateTime(v);
     }
 
     default LocalDate getLocalDate(K k) {
-        return Converts.toLocalDate(get(k));
+        return this.getLocalDate(k, Suppliers.nullSupplier());
     }
 
     default LocalDate getLocalDate(K k, LocalDate def) {
+        return this.getLocalDate(k, () -> def);
+    }
+
+    default LocalDate getLocalDate(K k, Supplier<LocalDate> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return Converts.toLocalDate(v);
     }
 
     default BigDecimal getBigDecimal(K k) {
-        return BigDecimals.toBigDecimal(get(k));
+        return this.getBigDecimal(k, Suppliers.nullSupplier());
     }
 
     default BigDecimal getBigDecimal(K k, BigDecimal def) {
+        return this.getBigDecimal(k, () -> def);
+    }
+
+    default BigDecimal getBigDecimal(K k, Supplier<BigDecimal> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return BigDecimals.toBigDecimal(v);
     }
 
     default BigInteger getBigInteger(K k) {
-        return BigIntegers.toBigInteger(get(k));
+        return this.getBigInteger(k, Suppliers.nullSupplier());
     }
 
     default BigInteger getBigInteger(K k, BigInteger def) {
+        return this.getBigInteger(k, () -> def);
+    }
+
+    default BigInteger getBigInteger(K k, Supplier<BigInteger> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return BigIntegers.toBigInteger(v);
     }
 
-    default <NK, NV> Map<NK, NV> getMap(String key) {
-        V v = get(key);
-        if (v instanceof Map) {
-            return (Map<NK, NV>) v;
-        } else {
-            return null;
-        }
-    }
-
-    default <NK, NV> Map<NK, NV> getMap(String key, Map<NK, NV> def) {
-        V v = get(key);
-        if (v instanceof Map) {
-            return (Map<NK, NV>) v;
-        } else {
-            return def;
-        }
-    }
-
-    default <NE> List<NE> getList(String key) {
-        V v = get(key);
-        if (v instanceof List) {
-            return (List<NE>) v;
-        } else {
-            return null;
-        }
-    }
-
-    default <NE> List<NE> getList(String key, List<NE> def) {
-        V v = get(key);
-        if (v instanceof List) {
-            return (List<NE>) v;
-        } else {
-            return def;
-        }
-    }
-
-    default <NE> Set<NE> getSet(String key) {
-        V v = get(key);
-        if (v instanceof Set) {
-            return (Set<NE>) v;
-        } else {
-            return null;
-        }
-    }
-
-    default <NE> Set<NE> getSet(String key, Set<NE> def) {
-        V v = get(key);
-        if (v instanceof Set) {
-            return (Set<NE>) v;
-        } else {
-            return def;
-        }
-    }
-
-    default <E> E getObject(K k, Function<V, E> f) {
-        Valid.notNull(f);
-        V v = get(k);
-        return v == null ? null : f.apply(v);
-    }
-
-    default <E> E getObject(K k, V def, Function<V, E> f) {
-        Valid.notNull(f);
-        V v = get(k);
-        return v == null ? f.apply(def) : f.apply(v);
-    }
-
     default <E> E getObject(K k) {
-        V v = get(k);
-        if (v == null) {
-            return null;
-        }
-        return (E) v;
+        return this.getObject(k, Suppliers.nullSupplier());
     }
 
     default <E> E getObject(K k, E def) {
+        return this.getObject(k, () -> def);
+    }
+
+    @SuppressWarnings("unchecked")
+    default <E> E getObject(K k, Supplier<E> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return (E) v;
     }
 
     default V get(K k, V def) {
+        return this.get(k, () -> def);
+    }
+
+    default V get(K k, Supplier<V> supplier) {
+        Valid.notNull(supplier);
         V v = get(k);
         if (v == null) {
-            return def;
+            return supplier.get();
         }
         return v;
     }
---
+++ orion-lang/src/test/java/com/orion/test/mutable/MutableMapTests.java
@@ -0,0 +1,151 @@
+package com.orion.test.mutable;
+
+import com.orion.lang.collect.MutableHashMap;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ * @author Jiahang Li
+ * @version 1.0.0
+ * @since 2022/1/23 14:36
+ */
+public class MutableMapTests {
+
+    private MutableHashMap<Integer, Integer> map;
+
+    private Integer key;
+
+    @Before
+    public void init() {
+        this.map = new MutableHashMap<>();
+        this.key = 0;
+        // map.put(key, 1);
+        map.put(key, null);
+    }
+
+    @Test
+    public void byteTests() {
+        System.out.println(map.getByte(key));
+        System.out.println(map.getByte(key, (byte) 0));
+        System.out.println(map.getByte(key, () -> (byte) 0));
+        System.out.println(map.getByteValue(key));
+        System.out.println(map.getByteValue(key, (byte) 0));
+        System.out.println(map.getByteValue(key, () -> (byte) 0));
+    }
+
+    @Test
+    public void shortTests() {
+        System.out.println(map.getShort(key));
+        System.out.println(map.getShort(key, (short) 0));
+        System.out.println(map.getShort(key, () -> (short) 0));
+        System.out.println(map.getShortValue(key));
+        System.out.println(map.getShortValue(key, (short) 0));
+        System.out.println(map.getShortValue(key, () -> (short) 0));
+    }
+
+    @Test
+    public void intTests() {
+        System.out.println(map.getInteger(key));
+        System.out.println(map.getInteger(key, 0));
+        System.out.println(map.getInteger(key, () -> 0));
+        System.out.println(map.getIntValue(key));
+        System.out.println(map.getIntValue(key, 0));
+        System.out.println(map.getIntValue(key, () -> 0));
+    }
+
+    @Test
+    public void longTests() {
+        System.out.println(map.getLong(key));
+        System.out.println(map.getLong(key, 0L));
+        System.out.println(map.getLong(key, () -> 0L));
+        System.out.println(map.getLongValue(key));
+        System.out.println(map.getLongValue(key, 0L));
+        System.out.println(map.getLongValue(key, () -> 0L));
+    }
+
+    @Test
+    public void floatTests() {
+        System.out.println(map.getFloat(key));
+        System.out.println(map.getFloat(key, 0F));
+        System.out.println(map.getFloat(key, () -> 0F));
+        System.out.println(map.getFloatValue(key));
+        System.out.println(map.getFloatValue(key, 0F));
+        System.out.println(map.getFloatValue(key, () -> 0F));
+    }
+
+    @Test
+    public void doubleTests() {
+        System.out.println(map.getDouble(key));
+        System.out.println(map.getDouble(key, 0D));
+        System.out.println(map.getDouble(key, () -> 0D));
+        System.out.println(map.getDoubleValue(key));
+        System.out.println(map.getDoubleValue(key, 0D));
+        System.out.println(map.getDoubleValue(key, () -> 0D));
+    }
+
+    @Test
+    public void booleanTests() {
+        System.out.println(map.getBoolean(key));
+        System.out.println(map.getBoolean(key, false));
+        System.out.println(map.getBoolean(key, () -> false));
+        System.out.println(map.getBooleanValue(key));
+        System.out.println(map.getBooleanValue(key, false));
+        System.out.println(map.getBooleanValue(key, () -> false));
+    }
+
+    @Test
+    public void charTests() {
+        System.out.println(map.getCharacter(key));
+        System.out.println(map.getCharacter(key, '0'));
+        System.out.println(map.getCharacter(key, () -> '0'));
+        System.out.println(map.getCharValue(key));
+        System.out.println(map.getCharValue(key, '0'));
+        System.out.println(map.getCharValue(key, () -> '0'));
+    }
+
+    @Test
+    public void stringTests() {
+        System.out.println(map.getString(key));
+        System.out.println(map.getString(key, "d"));
+        System.out.println(map.getString(key, () -> "d"));
+    }
+
+    @Test
+    public void dateTests() {
+        System.out.println(map.getDate(key));
+        System.out.println(map.getDate(key, new Date()));
+        System.out.println(map.getDate(key, Date::new));
+        System.out.println(map.getLocalDate(key));
+        System.out.println(map.getLocalDate(key, LocalDate.now()));
+        System.out.println(map.getLocalDate(key, LocalDate::now));
+        System.out.println(map.getLocalDateTime(key));
+        System.out.println(map.getLocalDateTime(key, LocalDateTime.now()));
+        System.out.println(map.getLocalDateTime(key, LocalDateTime::now));
+    }
+
+    @Test
+    public void numberTests() {
+        System.out.println(map.getBigDecimal(key));
+        System.out.println(map.getBigDecimal(key, BigDecimal.ONE));
+        System.out.println(map.getBigDecimal(key, () -> BigDecimal.valueOf(1L)));
+        System.out.println(map.getBigInteger(key));
+        System.out.println(map.getBigInteger(key, BigInteger.ONE));
+        System.out.println(map.getBigInteger(key, () -> BigInteger.valueOf(1L)));
+    }
+
+    @Test
+    public void getTests() {
+        System.out.println((Object) map.getObject(key));
+        System.out.println(map.getObject(key, BigDecimal.ONE));
+        System.out.println(map.getObject(key, () -> BigDecimal.valueOf(1L)));
+        System.out.println(map.get(key, 123));
+        System.out.println(map.get(key, () -> 123));
+    }
+
+}
